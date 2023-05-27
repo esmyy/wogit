cfbd15d432a508062217ade5d5c2c140c44e4d24
esmyy <iamesmyy@qq.com>
2023-05-27 09:52:32 +0800

    refactor: delete useless mirror

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # wogit
-git clone加速命令行工具，使用加速镜像解决从github克隆速度慢的问题，支持git命令透传，可以只在需要clone的时候使用，也可以替代git作为日常使用。
+
+git clone 加速命令行工具，使用加速镜像解决从 github 克隆速度慢的问题。
 
 ## 安装
 
@@ -8,13 +9,12 @@ npm i wogit -g
 ```
 
 ## 使用
-wogit的使用，除了支持几个指定特定源的选项之外，其他的与git一致，基本是把`git`替换成`wogit`即可
 
 ```bash
 # from
 git clone https://github.com/chalk/chalk.git
 
-# ti
+# to
 wogit clone https://github.com/chalk/chalk.git
 
 # 其他参数不变，跟使用git一样，如
@@ -22,7 +22,8 @@ wogit clone https://github.com/chalk/chalk.git --depth=1
 ```
 
 ## 镜像切换
-wogit默认使用cnpmjs镜像，使用 `wogit -h` 可以随时查看镜像切换的选项
+
+使用 `wogit -h` 查看镜像切换的选项
 
 ```sh
 ➜ wogit -h
@@ -30,30 +31,14 @@ Usage: wogit [options]
 
 Options:
   -V, --version   output the version number
-  -cn --cnpm      cnpmjs镜像(默认)
-  -fa --fastgit   fastgit镜像
   -ge --gitee     gitee镜像
   -gc --gitclone  gitclone镜像
-  -gh --github    使用原始github镜像
   -h, --help      display help for command
 ```
+
 比如
+
 ```bash
-# fastgit镜像
-wogit clone https://github.com/chalk/chalk.git -fa
-wogit clone https://github.com/chalk/chalk.git --fastgit
-
-# gitee镜像
 wogit clone https://github.com/chalk/chalk.git -ge
-wogit clone https://github.com/chalk/chalk.git -gitee
-
-# 其他不一而足
+wogit clone https://github.com/chalk/chalk.git --gitee
 ```
-
-说明：并非所有的仓库都有加速，请根据提示，确认是否需要使用`-gh`强制直接使用github。
-
-## 说明
-wogit只会在 **操作为clone且远程为github仓库** 时才会使用加速的源，其他情况是透传的，因此clone外的其他操作也是支持的，可以替代git作为日常使用。
-
-当然，也有不好的地方——**命令多了两个字符** 😂  没办法，短的名字都被早早占了坑了。
-
--- index.js
+++ index.js
@@ -1,52 +1,54 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const { program } = require('commander');
-const shelljs = require('shelljs');
-const pkg = require(path.resolve(__dirname, 'package.json'));
+const path = require("path");
+const { program } = require("commander");
+const shelljs = require("shelljs");
+const pkg = require(path.resolve(__dirname, "package.json"));
 
 const GH_ADDR_MATCH_REG = /(https?:\/\/github\.com)(\/.*\/)+([^\/]+\.git)/;
 const GH_TEST_REG = /https?:\/\/github\.com\/.*.git/;
 const MIRROR_DICT = {
-  cnpm: 'https://github.com.cnpmjs.org',
-  gitee: 'https://gitee.com/mirrors',
-  gitclone: 'https://gitclone.com/github.com',
-  fastgit: 'https://hub.fastgit.org',
-  github: 'https://github.com',
+  gitee: "https://gitee.com/mirrors/",
+  gitclone: "https://gitclone.com/github.com",
 };
 const MIRROR_LIST = Object.keys(MIRROR_DICT);
 
 program
   .version(pkg.version)
   .allowUnknownOption()
-  .option('-cn --cnpm', 'cnpmjs镜像(默认)')
-  .option('-fa --fastgit', 'fastgit镜像')
-  .option('-ge --gitee', 'gitee镜像')
-  .option('-gc --gitclone', 'gitclone镜像')
-  .option('-gh --github', '使用原始github镜像')
+  .option("-ge --gitee", "gitee 镜像")
+  .option("-gc --gitclone", "gitclone 镜像")
   .parse();
 
 /**
  * convert a git command to new mirror
- * @param {string} originCmd 
- * @param {} usedMirror 
+ * @param {string} originCmd
+ * @param {} usedMirror
  */
 function convertCmd(originCmd, usedMirror) {
   let cmd = originCmd;
   let matchedArr = cmd.match(GH_ADDR_MATCH_REG);
-  let repoAddrObj = matchedArr ? {
-    url: matchedArr[0],
-    domain: matchedArr[1],
-    path: matchedArr[2],
-    name: matchedArr[3],
-  } : null;
+  let repoAddrObj = matchedArr
+    ? {
+        url: matchedArr[0],
+        domain: matchedArr[1],
+        path: matchedArr[2],
+        name: matchedArr[3],
+      }
+    : null;
 
   if (repoAddrObj) {
     // path not the same on gitee
-    if (usedMirror === 'gitee') {
-      cmd = cmd.replace(repoAddrObj.url, MIRROR_DICT[usedMirror] + repoAddrObj.name);
+    if (usedMirror === "gitee") {
+      cmd = cmd.replace(
+        repoAddrObj.url,
+        MIRROR_DICT[usedMirror] + repoAddrObj.name
+      );
     } else {
-      cmd = cmd.replace(repoAddrObj.url, MIRROR_DICT[usedMirror] + repoAddrObj.path + repoAddrObj.name);
+      cmd = cmd.replace(
+        repoAddrObj.url,
+        MIRROR_DICT[usedMirror] + repoAddrObj.path + repoAddrObj.name
+      );
     }
   }
 
@@ -55,31 +57,30 @@ function convertCmd(originCmd, usedMirror) {
 
 /**
  * get the mirror name
- * @param {object} program 
- * @param {array} mirrorList 
+ * @param {object} program
+ * @param {array} mirrorList
  */
 function getUsedMirror(program, mirrorList) {
-  const index = mirrorList.findIndex(function(key) {
+  const index = mirrorList.findIndex(function (key) {
     return program[key];
   });
 
-  return index >= 0 ? mirrorList[index] : '';
+  return index >= 0 ? mirrorList[index] : "";
 }
 
 const args = program.parse(process.argv).args;
-let cmd = args.join(' ');
-let usedMirror = '';
+let cmd = args.join(" ");
+let usedMirror = "";
 
 // only when repo was clone from github needs to change mirror
 // two points: only github, only clone
 if (/(^|\s)clone\s/.test(cmd) && GH_TEST_REG.test(cmd)) {
-  usedMirror = getUsedMirror(program, MIRROR_LIST) || 'cnpm';
+  usedMirror = getUsedMirror(program, MIRROR_LIST) || "gitclone";
   cmd = convertCmd(cmd, usedMirror);
-  // console.log('更换镜像后的命令: ', cmd);
 }
 
-cmd = 'git ' + cmd;
-cmd = cmd.replace(/\sclone\s/, ' clone --progress ');
-// console.log('cmd', program.cnpm, program.fastgit, program.gitclone, program.gitee, program.github,);
+cmd = "git " + cmd;
+cmd = cmd.replace(/\sclone\s/, " clone --progress ");
 
+console.log("cmd", cmd);
 shelljs.exec(cmd);
